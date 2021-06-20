59734760ba20e17b09a08414da209c5b13c6c141
Jaden Padua <paduajaden@gmail.com>
2021-06-19 19:15:47 -0700

    Mock GQL Backend w / mutations & queries

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const PORT = 8080;
+const { graphqlHTTP } = require("express-graphql")
+
+const schema = require("./Schemas")
+
+app.use("/graphql", graphqlHTTP({
+    schema,
+    graphiql: true
+}))
+
+app.listen(PORT, () => {
+    console.log("Listening on Port: " + PORT)
+})
